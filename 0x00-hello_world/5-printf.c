f4d9783e83124fbbce80e5af42c878493b5e8c50
Obinna Ukwa <obikelv@gmail.com>
2023-02-16 14:48:53 -0800

    Hello, printf3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main- prints a string followed by a new line
- * Return 0
- * */
+ * main - A program that print a line using the printf function
+ * Return: 0 (success)
+ */
 int main(void)
 {
-printf(`"with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
