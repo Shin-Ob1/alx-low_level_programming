4ebc37a0604ea203c98a6dee95b737c61371798d
Obinna Ukwa <obikelv@gmail.com>
2023-02-17 19:16:49 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,24 +17,23 @@
  */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < 100)
-    {
-        if (i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
+	while (i < 100)
+	{
+		if (i / 10 < i % 10)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
-    }
-
-    putchar('\n');
-    return (0);
+			if (i < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
