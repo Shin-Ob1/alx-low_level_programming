5ca511d935687bda92bbf35d4cacd23540a18f37
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 01:27:49 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers-prints numbers from 0 to 0
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; 1 <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+}
