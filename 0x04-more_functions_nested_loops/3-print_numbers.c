4f412c2853a6a125509b7cd39f88059dbbb963c6
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 01:10:38 +0000

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers-prints numbers from 0 to 0
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 1 <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
