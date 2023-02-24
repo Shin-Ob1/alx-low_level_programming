918524cb473cf5c109d12a35d8905ba9a27f47b3
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 01:00:20 +0000

    collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul-prints the absolute value of an integer.
+ *@a:the integer to compute its absolute value.
+ * @b: the other integer
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	return (x);
+}
