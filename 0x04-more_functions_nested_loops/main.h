5bce0e7290a383efa1d3ae062fb5ae66a9da395f
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 01:34:11 +0000

    numbers minus 2 and 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
  *  * The function would print one character
  *   * The function name is _putchar
  *    */
-int _putchar(char c);
+int _putchar(int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
