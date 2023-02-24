38b620b29329b63acf21461acae23305c6717f5d
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 02:16:27 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * more_numbers - prints 10 times numbers
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i = 1, j;
+
+	while (i <= 10)
+	{
+		j = 0
+		while (j <= 14)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
