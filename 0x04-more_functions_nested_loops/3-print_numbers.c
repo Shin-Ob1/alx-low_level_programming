91f267210d0d75adb6f6c52609e09b804296bfc2
Obinna Ukwa <obikelv@gmail.com>
2023-02-24 06:21:18 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers-prints numbers from 0 to 0
- *
- * Return: Always 0
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	char i = '0';
+	char c;
 
-	while (i = '9')
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
-		i++;
+		_putchar(c);
 	}
 	_putchar('\n');
 }
