b6bc68908b7bc29ad069ab9c0145a148390f9990
Obinna Ukwa <obikelv@gmail.com>
2023-02-27 13:21:32 -0800

    1swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,14 @@
 
 /**
  * swap_int - swaps two integers
- * @a is the first integer
- * @b is the second integer
+ * @a: the first integer
+ * @b: is the second integer
  * Return: Always 0
  */
 
-swap_int(int *a int *b)
+void swap_int(int *a, int *b)
+
 {
-	a = b;
-	b = a;
+	*b = a;
+	*a = b;
 }
