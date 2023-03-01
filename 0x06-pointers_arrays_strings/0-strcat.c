9193e813b4f12369a6a0255fb0b03786a66aec21
Obinna Ukwa <obikelv@gmail.com>
2023-03-01 09:33:43 -0800

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: is the first string
+ * @src: is the second string
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = strlen(dest), k = strlen(src);
+
+	for (i = 0; i < k; i++)
+	{
+		dest[j + i] = src[i];
+	}
+
+	Return(0);
+}
