196f64d6b620603519b369fcfa5f6347951932ea
Obinna Ukwa <obikelv@gmail.com>
2023-03-06 13:12:31 -0800

    memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _memset - Sets a constant character in memory
+ * @s: input value
+ * @b: input value
+ * @n: input value
+ *
+ * Return: i
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
