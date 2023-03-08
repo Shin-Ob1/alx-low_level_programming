bc86fed218112331c52e88fcdacb6f9ec2e64e32
Obinna Ukwa <obikelv@gmail.com>
2023-03-08 08:58:28 -0800

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _puts_recursion - prints in recursion
+* _print_rev_recursion - prints reverse in recursion
 * @s: input string
 * Return:Always void
 */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
