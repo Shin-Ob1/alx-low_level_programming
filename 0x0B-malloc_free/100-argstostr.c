08a6f7942ce8f7be90c69ddb70b988969740fb55
Obinna Ukwa <obikelv@gmail.com>
2023-03-14 16:52:27 -0700

    argst

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,43 @@
-#include <stdlib.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * *argstostr - concatenates all arguments of program
- *
- * @ac: argument count
- * @av: point to pointer to arguments
- * Return: pointer to new string or NULL if failed
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, k, n, size;
 	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	while (a < ac)
 	{
-		for (k = 0; av[i][k] != '\0'; k++)
-			;
-		size += k + 1;
-	}
-	str = malloc(size + 1);
-	if (str == 0)
-		return (NULL);
-	n = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (k = 0; av[i][k] != '\0'; k++)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			str[n] = av[i][k];
-			n++;
+			count++;
+			b++;
 		}
-		str[n] = '\n';
-		n++;
+		a++;
+	}
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
 	}
 	return (str);
 }
