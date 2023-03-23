cbbcb58cbfea19db80d895a4e43de19e5c2d5f76
Obinna Ukwa <obikelv@gmail.com>
2023-03-23 15:05:35 -0700

    sum variable inputs

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all inputed variables
+ *
+ * @n: input value
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	int sum = 0, i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VARIADIC_FUNCTION_H_
+#define _VARIADIC_FUNCTION_H_
+
+int _putchar(int);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
