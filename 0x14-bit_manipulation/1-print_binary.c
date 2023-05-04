69f54ee06aa2dc182725e5bdf9dc5929bcd37a24
Obinna Ukwa <obikelv@gmail.com>
2023-05-04 13:13:15 -0700

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary of a number
+ * @n: input unsigned long integer
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, bits;
+	unsigned long int shifts;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	bits  = 0;
+	shifts = n;
+	while (shifts != 0)
+	{
+		shifts = shifts>>1;
+		bits++;
+	}
+	for (i = (bits - 1); i >= 0; i--)
+	{
+		if ((n>>i) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+	return;
+}
