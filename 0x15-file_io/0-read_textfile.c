f8365f66e5495fb6f59b2c4a845d3b095958c654
Obinna Ukwa <obikelv@gmail.com>
2023-05-08 16:10:28 -0700

    Copy file content to another

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
+	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (fd == -1 || r == -1 || w == -1 || w != r)
