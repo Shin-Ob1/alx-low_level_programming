4d5ec4a00112fae9cf8a170d3ef7d574efa4badd
Obinna Ukwa <obikelv@gmail.com>
2023-05-08 16:13:08 -0700

    Debugging 0-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,13 +4,14 @@
 /**
  * read_textfile - reads a textfile then it prints to POSIX stdout
  * @filename: A pointer to file name
- * @letters: The numbers of letters that functions reads and prints
+ * @letters: The numbers of letters that the functions reads and prints
  *
- * Return: the actual number of bytes the function can read ad print.
+ * Return: if function fails or filename is NULL - 0.
+ *O/w - the actual number of bytes the function can read ad print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, r, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -20,18 +21,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	r = read(fd, buffer, letters);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (fd == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(fd);
+	close(o);
 
 	return (w);
 }
